2f22de715807aa82a22f0922fdc8d9e6bdbf5d6b
PrathamModi001 <prathammodi001@gmail.com>
2024-04-06 10:01:57 +0530

    signup modal

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,19 +1,65 @@
 import React from 'react'
+import { useState } from 'react'
 
-const AuthModal = ({setShowModal}) => {
+const AuthModal = ({ setShowModal }) => {
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [confirmPassword, setConfirmPassword] = useState(null)
+    const [error, setError] = useState(null)
+
+    const isSignUp = true;
 
     const handleClick = () => {
         setShowModal(false);
     }
 
-    const isSignUp = true;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        try{
+            if(isSignUp && password !== confirmPassword){
+                setError('Passwords do not match')
+            }
+        } catch(err){
+            console.log(err)
+        }
+    }
+
+
 
     return (
         <div className='auth-modal'>
             <div className='close-icon' onClick={handleClick}>ⓧ</div>
 
-            <h2>{isSignUp? 'CREATE ACCOUNT': 'LOG IN'}</h2>
-            Auth Modal
+            <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="email"
+                    required={true}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="password"
+                    required={true}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                {/* if we are signing up only then show this warna dont show */}
+                {isSignUp && <input
+                    type="password"
+                    id="password-check"
+                    name="password-check"
+                    placeholder="confirm password"
+                    required={true}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />}
+                <input className="secondary-button" type="submit" />
+                <p>{error}</p>
+            </form>
         </div>
     )
 }
